eaa07dad8f7da6d46ed5c45c2039da70f82eb0b1
LorenaBarr <karenlbarrera@gmail.com>
2024-10-20 23:21:52 -0500

    modificacion del campo usuario en el modelo ventas, para hacer las pruebas

--- inventario_project/inventario/models/venta.py
+++ inventario_project/inventario/models/venta.py
@@ -1,33 +1,26 @@
 from django.db import models, transaction
-from .product import Product  # Importación relativa del modelo Product
-from .bodega import Bodega    # Importación relativa del modelo Bodega
-from .inventario import Inventario  # Asegúrate de importar Inventario
+from .product import Product
+from .bodega import Bodega
+from .inventario import Inventario
 from django.core.exceptions import ValidationError
 from django.db.models import F
-from django.contrib.auth.models import User
 
 class Venta(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     bodega = models.ForeignKey(Bodega, on_delete=models.CASCADE)
-    cantidad = models.PositiveIntegerField()  # Solo valores positivos
+    cantidad = models.PositiveIntegerField()
     fecha = models.DateTimeField(auto_now_add=True)
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)  # Relación con User
 
     def __str__(self):
         return f"Venta: {self.product.nombre} - {self.bodega.nombre} - {self.cantidad}"
 
     def clean(self):
-        # Validación para asegurar que la cantidad vendida no exceda el stock disponible
         inventario = Inventario.objects.filter(product=self.product, bodega=self.bodega).first()
         if not inventario or inventario.stock < self.cantidad:
             raise ValidationError(f"No hay suficiente stock disponible para {self.product.nombre} en la bodega {self.bodega.nombre}. Disponible: {inventario.stock if inventario else 0}")
 
     @transaction.atomic
     def save(self, *args, **kwargs):
-        self.clean()  # Llamamos la validación antes de guardar
-
-        # Guardamos la venta
+        self.clean()
         super(Venta, self).save(*args, **kwargs)
-
-        # Reducir el stock después de realizar la venta
         Inventario.objects.filter(product=self.product, bodega=self.bodega).update(stock=F('stock') - self.cantidad)
